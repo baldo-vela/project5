441c44c28046005b58d11fae13f947e06c1bb809
baldo-vela <baldo.vela+github@gmail.com>
2021-06-28 13:54:03 -0500

    Adds some ToDo's

--- src/components/TrackCard.js
+++ src/components/TrackCard.js
@@ -3,7 +3,10 @@ import React from 'react'
 const TrackCard = (props) => {
     return(
         <div className='track-card'>
-            
+            {/* Track Name
+            Track Artist
+            Album Cover Image
+            Player Link? */}
         </div>
     )
 }
