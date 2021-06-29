f7047f24b374f9fbcd7cc33eab5bf454bb1dbbaf
baldo-vela <baldo.vela+github@gmail.com>
2021-06-29 17:30:42 -0500

    Renames files to better match conventions

---
+++ src/containers/NavBar.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+class NavBar extends Component {
+    render () {
+        return(
+            <div className='navbar'>
+                <ul>
+                    <li>Main</li>
+                    <li>My Playlists</li>
+
+                </ul>
+            </div>
+        )
+    }
+}
+
+export default NavBar
