521f9ff8ca742c7a32e244d0ba5f2c7fd050e943
baldo-vela <baldo.vela+github@gmail.com>
2021-07-15 20:38:07 -0500

    Configures Front End for authorization request

--- src/actions/Login.js
+++ src/actions/Login.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import SplashLogo from '../assets/images/animated_splash.gif';
 import '../stylesheets/Login.css';
+import { loginUrl } from '../configurations/Spotify'
 
 
 // Allows user to sign in with Spotify
 export default function SignIn() {
-    //TODO: Add Spotify sign in
+    
     return (
         <div className="login">
             <img src={SplashLogo} alt="Animated Greatest Hits Logo" />
-            <a href='#'>Login with Spotify</a>
+            <a href="http://localhost:8000/api/v1/login">Login with Spotify</a>
             
         </div>
     )
--- src/configurations/Spotify.js
+++ src/configurations/Spotify.js
@@ -1,14 +1,20 @@
+//where we need to authenticate using Spotify
 const authEndpoint = "https://accounts.spotify.com/authorize";
-const redirectUri = "http://localhost:3000/callback";
 
-const clientId = process.env.SPOTIFY_APP_CLIENT_ID_KEY;
+//Where we told the Spotify API to redirect us back to
+const redirectUri = "https://localhost:3000/";
 
+//Spotify's client ID, shimmed from .env
+const clientId = process.env.REACT_APP_SPOTIFY_APP_CLIENT_ID_KEY;
+console.log('Client Id:', clientId);
+//Requested Permissions from Spotify
 const scopes = [  
-    "playlist-read-collaborative",
-    "playlist-read-private",
-    "playlist-read-public",
-    "streaming",
-    "user-library-read",
-    "user-read-private",
-    "user-top-read"
+  "streaming",
+  "user-read-email",
+  "user-read-private",
 ];
+
+//The final Auth URL
+export const loginUrl = `${authEndpoint}?client_id=${clientId}&response_type=code&redirect_uri=${redirectUri}&scope=${scopes.join(
+    "%20"
+  )}`;
