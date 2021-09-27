c01c91ee089a2e6d942fa4d1acd997144835b01c
baldo-vela <baldo.vela+github@gmail.com>
2021-09-27 12:27:46 -0500

    Prunes extraneous content

--- src/configurations/Spotify.js
+++ src/configurations/Spotify.js
@@ -2,19 +2,8 @@
 const authEndpoint = "https://accounts.spotify.com/authorize";
 
 //Where we told the Spotify API to redirect us back to
-const redirectUri = "https://localhost:3000/";
+const redirectUri = "https://localhost:3001/";
 
-//Spotify's client ID, shimmed from .env
-const clientId = process.env.REACT_APP_SPOTIFY_APP_CLIENT_ID_KEY;
-console.log('Client Id:', clientId);
-//Requested Permissions from Spotify
-const scopes = [  
-  "streaming",
-  "user-read-email",
-  "user-read-private",
-];
 
 //The final Auth URL
-export const loginUrl = `${authEndpoint}?client_id=${clientId}&response_type=code&redirect_uri=${redirectUri}&scope=${scopes.join(
-    "%20"
-  )}`;
+export const usersAPI = `http://localhost:3000/api/v1/users`;
