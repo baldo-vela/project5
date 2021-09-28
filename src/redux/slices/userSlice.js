9600a6b5b6b787e2553957fd8b8fa23ffd57b391
baldo-vela <baldo.vela+github@gmail.com>
2021-09-27 21:45:39 -0500

    Refactors for better clarity

--- src/redux/slices/userSlice.js
+++ src/redux/slices/userSlice.js
@@ -1,20 +1,6 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 // TODO rewrite this to reference our Rails API backend
 import { usersAPI } from '../../configurations/Spotify';
-/*  Sample User Data:
-    User id: 1, 
-    name: "foo", 
-    email: nil, 
-    href: "https://api.spotify.com/v1/users/foo", 
-    country: "US", 
-    spotify_url: "https://open.spotify.com/user/foo", 
-    spotify_id: "foo", 
-    spotify_access_token: [FILTERED], 
-    spotify_refresh_token: [FILTERED], 
-    uri: "spotify:user:foo", 
-    image_url: "https://i.scdn.co/image/ab6775700000ee855740709a5b...", 
-    created_at: "2021-07-16 01:45:30.488108000 +0000", 
-    updated_at: "2021-07-16 02:15:29.123883000 +0000">] */
 
 /*  If this was pure javascript, we would have to use the following:
     async function fetchUser() {
@@ -24,6 +10,8 @@ import { usersAPI } from '../../configurations/Spotify';
         console.log("User Data: ", data);
         return data;
     }
+
+    However with RKT we can use the following: 
     export const loadUser() = createAsyncThunk(
         "currentUser/loadUser",
         async (thunkAPI) => {
@@ -40,6 +28,9 @@ export const loginUser = createAsyncThunk(
   async () => {
       console.log('Fetching User Login from: ', usersAPI)
       const data = await fetch(usersAPI);
+      /*
+        method: "GET"
+      */
       const json = await data.json();
       return json;
   }
@@ -77,26 +68,41 @@ const sliceOptions = {
     }
 }
 
-const initialState = {}
-//TODO: Refactor this to be included in a single Root Reducer call
+/*  Sample User Data:
+    User id: 1, 
+    name: "foo", 
+    email: nil, 
+    href: "https://api.spotify.com/v1/users/foo", 
+    country: "US", 
+    spotify_url: "https://open.spotify.com/user/foo", 
+    spotify_id: "foo", 
+    spotify_access_token: [FILTERED], 
+    spotify_refresh_token: [FILTERED], 
+    uri: "spotify:user:foo", 
+    image_url: "https://i.scdn.co/image/ab6775700000ee855740709a5b...", 
+    created_at: "2021-07-16 01:45:30.488108000 +0000", 
+    updated_at: "2021-07-16 02:15:29.123883000 +0000">] 
+*/
+const initialState = { 
+    user_id: '',
+    spotify_id: '',
+    email: '',
+    href: '',
+    country: '',
+    spotify_url: '',
+    spotify_access_token: '',
+    spotify_refresh_token: '',
+    uri: '',
+    image_url: '',
+    created_at: '',
+    updated_at: '',
+    isLoading: false,
+    hasError: false
+}
+
 const userSlice = createSlice({
     name: "user",
-    initialState: {
-        user_id: '',
-        spotify_id: '',
-        email: '',
-        href: '',
-        country: '',
-        spotify_url: '',
-        spotify_access_token: '',
-        spotify_refresh_token: '',
-        uri: '',
-        image_url: '',
-        created_at: '',
-        updated_at: '',
-        isLoading: false,
-        hasError: false
-        },
+    initialState: { initialState },
     reducers: {
         setCurrentUser: (state, action) => {
             state.user_id = action.payload.user_id;
