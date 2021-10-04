cb0a7155ed91106cd912eab11b6e9dccbbf55437
baldo-vela <74266847+baldo-vela@users.noreply.github.com>
2021-10-04 13:05:15 -0500

    Adds configurations for SpotifyAuth

--- src/configurations/Spotify.js
+++ src/configurations/Spotify.js
@@ -2,8 +2,25 @@
 const authEndpoint = "https://accounts.spotify.com/authorize";
 
 //Where we told the Spotify API to redirect us back to
-const redirectUri = "https://localhost:3001/";
-
-
+export const redirectUri = "https://localhost:3001/";
+//Our Client URL
+export const clientURL = "https://localhost:3000/";
+//Our backend API server location
+export const apiURL = 'http://localhost:3001/api/v1/login';
+//Spotify API scopes
+export const scopes = [
+    'user-library-read',
+    'playlist-read-collaborative',
+    'playlist-modify-private',
+    'user-modify-playback-state',
+    'user-read-private',
+    'user-top-read',
+    'playlist-modify-public',
+    'user-read-recently-played',
+    'user-read-playback-state',
+    'user-read-currently-playing'
+    ];
+//Public Spotify Client ID
+export const clientID = "6ed60468af744e8699f512dc88fcd567";
 //The final Auth URL
 export const usersAPI = `http://localhost:3000/api/v1/users`;
