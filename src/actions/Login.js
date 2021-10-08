84973bfcd1787b242bd706234b30bf4468095ad8
baldo-vela <74266847+baldo-vela@users.noreply.github.com>
2021-10-08 13:19:09 -0500

    Updates fetching a user direct from Spotify

--- src/actions/Login.js
+++ src/actions/Login.js
@@ -10,43 +10,56 @@ import {
 //External Libraries
 import { SpotifyAuth } from 'react-spotify-auth';
 //Configuration File for Spotify
-import { apiURL, clientURL, clientID, redirectUri, scopes } from '../configurations/Spotify';
+import { apiURL, clientURL, clientID, redirectUri, scopes, spotifyUserURL } from '../configurations/Spotify';
 
 //Stylesheet for this component
 import SplashLogo from '../assets/images/animated_splash.gif';
 import '../stylesheets/Login.css';
 
 export class Login extends React.Component {
-    // dispatch = useDispatch();
-
-    // async apiResponseHandler() {
-    //     try{
-    //         let resp = await fetch('http://localhost:3001/api/v1/login', 
-    //             {   
-    //                 body: null,
-    //                 headers: {
-    //                     'Content-Type': 'application/json',
-    //                     'Accept': 'application/json'
-    //                 },
-    //                 method: 'POST',
-    //                 mode: 'cors',
-    //                 redirect: 'follow'
-    //             }
-    //         ); 
-    //         if(!resp.ok) {
-    //             throw new Error(resp.statusText);
-    //         }
-    //         console.log(resp);
-    //     } catch (error) {
-    //         console.log(error);
-    //     }
-    //     //this.props.dispatch(setCurrentUser(resp));
-    // }
-
-    authHandler = (token) => {
+    constructor() {
+        super();
+        this.state = {
+            token: null,
+            error: null,
+            loading: false,
+            user: [],
+        };
+    }
+    async fetchUser(token) {
+        // TODO - Fetch user from the Spotify API, push user from Spotify to our backend
+        // TODO - Set the current user in the Redux store
+        
+        try {
+            console.log("Fetching user...", token);
+            let resp = await fetch(spotifyUserURL, 
+                {
+                    body: null,
+                    headers: {
+                        'Authorization': "Bearer " + token,
+                        'Content-Type': 'application/json'
+                    },
+                    // method: 'GET',
+                    // mode: 'cors',
+                }
+            );
+            if(!resp.ok) {
+                throw new Error(resp.statusText);
+            }
+            let data = await resp.json();
+            console.log("User Fetched");
+            console.table(data);
+        } catch (error) {
+            console.trace(error);
+        }
+    }
+    tokenHandler = (token) => {
         //Note .trace ouputs to the console with where the function is called
-        console.trace('authHandler recieving access token ', token);
-        console.log('Fetching User')
+        console.log('authHandler recieving access token ', token);
+        //TODO Fix Setting the token in the state
+        this.setState({ token: token });
+        this.fetchUser(token);
+        
     }
 
     render() {
@@ -62,7 +75,8 @@ export class Login extends React.Component {
                     redirectUri={clientURL}
                     clientID={clientID}
                     scopes={scopes}
-                    onAccessToken={this.authHandler}
+                    onAccessToken={this.tokenHandler}
+                    noCookie={true}
                 />
             </div>
         );
