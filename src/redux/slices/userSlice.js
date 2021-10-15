67f0d397147ba8f9e18dbf6295e3e298415a5058
baldo-vela <74266847+baldo-vela@users.noreply.github.com>
2021-10-15 17:06:03 -0500

    Adds a null error value to user object

--- src/redux/slices/userSlice.js
+++ src/redux/slices/userSlice.js
@@ -63,6 +63,7 @@ const sliceOptions = {
         isLoggedIn: false,
         isLoading: false,
         hasError: false,
+        error: null,
     },
     reducers: {
         setAuthToken(state, action) {
