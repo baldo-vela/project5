a81f2a82fb48f1d96c89cd3341705e7844aec16d
baldo-vela <74266847+baldo-vela@users.noreply.github.com>
2021-10-18 12:25:56 -0500

    Checks off a ToDo and adds more documentation.

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,9 +3,9 @@ import { configureStore } from '@reduxjs/toolkit';
 import userSlice from './slices/userSlice';
 import userSliceReducer, { setAuthToken } from './slices/userSlice';
 //TODO: Depreciate this and move it's functions to index
+// Note: Thanks to RTK #configureStore(): wraps createStore to provide simplified configuration options and good defaults. It can automatically combine your slice reducers, adds whatever Redux middleware you supply, includes redux-thunk by default, and enables use of the Redux DevTools Extension.
 export default configureStore ({
     reducer: {
-        //TODO Replace this with a root reducer
         userSliceReducer: userSlice
     },
     devTools: process.env.NODE_ENV !== 'production',
