7fdace7e8879b08c95bec716d9c84d892f59d7e9
baldo-vela <74266847+baldo-vela@users.noreply.github.com>
2021-10-19 12:56:24 -0500

    Cleans up deprecated code

--- src/actions/Login.js
+++ src/actions/Login.js
@@ -32,72 +32,16 @@ export const Login = () => {
     //     };
     // }
     const dispatch = useDispatch();
-    //const { hasError } = useSelector((state) => state.currentUser);
+    const { hasError } = useSelector((state) => state.currentUser);
 
-    /* async fetchUser(token) {
-        // Fetches user from the Spotify API
-        // TODO push user from Spotify to our backend
-        // TODO - Set the current user in the Redux store
-        
-        try {
-            //console.log("Fetching user...", token);
-            let resp = await fetch(spotifyUserURL, 
-                {
-                    body: null,
-                    headers: {
-                        'Authorization': "Bearer " + token,
-                        'Content-Type': 'application/json'
-                    },
-                    // method: 'GET',
-                    // mode: 'cors',
-                }
-            );
-            if(!resp.ok) {
-                throw new Error(resp.statusText);
-            }
-            let data = await resp.json();
-            console.log("User Fetched");
-            console.log(data);
-        } catch (error) {
-            console.trace(error);
-        }
-    } */
-    // async fetchPlaylists(token) {
-    //     // Fetches user's playlists from the Spotify API
-    //     // TODO push user's playlists from Spotify to app Store
-    //     try {
-    //         let resp = await fetch(spotifyUserURL + "/playlists", 
-    //             {
-    //                 body: null,
-    //                 headers: {
-    //                     'Authorization': "Bearer " + token,
-    //                     'Content-Type': 'application/json'
-    //                 },
-    //             }
-    //         );
-    //         if(!resp.ok) {
-    //             throw new Error(resp.statusText);
-    //         }
-    //         let data = await resp.json();
-    //         console.log("Playlists Fetched");
-    //         console.log(data);
-
-    //     } catch (error) {
-    //         console.trace(error);
-    //     }
-    // }
-
+    
     const tokenHandler = (token) => {
-        //Note .trace ouputs to the console with where the function is called
+        //Note .trace outputs to the console with where the function is called
         console.log('authHandler recieving access token ', token);
         //TODO Fix Setting the token in the state
         dispatch(setAuthToken(token))
         dispatch(fetchUser(token))
         dispatch(fetchPlaylists(token))
-
-        //this.fetchUser(token);
-        //this.fetchPlaylists(token);
-        
     }
 
 
