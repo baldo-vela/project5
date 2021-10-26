c30381caa603683033b4e447869fb95e724d4a6c
baldo-vela <74266847+baldo-vela@users.noreply.github.com>
2021-10-26 18:08:26 -0500

    Adds a selector to pull  playlists into dashboard

--- src/containers/Dashboard.js
+++ src/containers/Dashboard.js
@@ -2,11 +2,16 @@
 // Ideally states the username, their user icon, and their playlists
 
 import React from 'react'
+
+//For creating dummy data
 import faker from 'faker'
+
+//For Rendering the list of playlists
 import { FixedSizeList as List } from 'react-window';
 import AutoSizer from 'react-virtualized-auto-sizer';
 
 import '../stylesheets/Dashboard.css'
+import { useSelector } from 'react-redux';
 
 // Dummy Data
 const data = new Array(100).fill().map((value, id) => ({
@@ -16,7 +21,10 @@ const data = new Array(100).fill().map((value, id) => ({
     }))
 
 const Dashboard = ({}) => {
-    
+    //Selects the user's playlists from the Redux store
+    const playlists = useSelector((state) => state.playlists);
+
+
     const playlistItem = ({ index, key, style }) => (
         <div>
             <div key={key} style={style} className="playlist-preview">
