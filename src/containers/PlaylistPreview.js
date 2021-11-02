c175f22192e490d0172f20753ba946a13e81ef30
baldo-vela <74266847+baldo-vela@users.noreply.github.com>
2021-11-02 16:24:58 -0500

    Adds a push to url for playlist details

--- src/containers/PlaylistPreview.js
+++ src/containers/PlaylistPreview.js
@@ -1,4 +1,5 @@
-import React, {Component} from 'react'
+import React, {Component} from 'react';
+import history from '../history';
 
 class PlaylistPreview extends Component {
     constructor(){
@@ -7,9 +8,9 @@ class PlaylistPreview extends Component {
             isLoading: true,
         }
     }
-    // linkToPlayer = () =>{
-
-    // }
+    viewDetails = () =>{
+        history.push(`/playlist/${this.props.playlist.id}`);
+    }
     render() {
         //TODO: Fix issue with <p> tags in descrip:
             //<p>Enter another dimension of classical music with these atmospheric Sci-fi soundtracks. Twin playlist: <a href="spotify:user:spotify:playlist:37i9dQZF1DX0i61tT0OnnK">Space-themed Classical Music</a>.</p>
@@ -17,7 +18,7 @@ class PlaylistPreview extends Component {
         const { id, name, description, owner, spotifyLink } = this.props.playlist;
         return(
             <div className='playlist-preview' key={`playlist-${id}`}>
-                <h1><href className='playlist-preview-name'>{name}</href></h1>
+                <h1><href onClick={this.viewDetails}className='playlist-preview-name'>{name}</href></h1>
                 <p>{description ? description : '' }</p>
                 <p>{spotifyLink}</p>
                 <p>id: {id}</p>
