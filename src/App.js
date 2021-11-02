dec1907cb156f26f1e0983bd704d87e443014fad
baldo-vela <74266847+baldo-vela@users.noreply.github.com>
2021-11-02 16:26:30 -0500

    Adds simple BrowserHistory

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 //React Dependancies
 import React from 'react'
 //import { connect, useDispatch, useSelector } from 'react-redux';
-import { BrowserRouter as Router, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Route, } from 'react-router-dom';
 
 //Style Imports
 import './stylesheets/App.css';
 import './stylesheets/index.css';
 
 //Root Imports
-import Login from './actions/Login.js'
+import Login from './actions/Login.js';
+import history from './history';
 
 
 //Container Imports
@@ -16,7 +17,7 @@ import Dashboard from './containers/Dashboard';
 // TODO: move this to Main component import Nav from './containers/Nav'
 
 function App(){
-    
+    //let history = useHistory();
     //const dispatch = useDispatch();
     //const isLoggedIn = useSelector(isLoggedIn);
     //const user = props.user;
@@ -27,7 +28,10 @@ function App(){
                 
                 <Route path='/' exact component={Login} /> 
                 {/* TODO: Set this route below to be private? */}
-                <Route path='/main' exact component={Dashboard} />
+                <Route path='/main' render={((props)=> (
+                    <Dashboard {...props} history={history} />
+                ))} />
+                
                 
             </Router>
         </div>
