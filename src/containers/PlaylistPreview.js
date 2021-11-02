c83dfd2985a0d90075d8120b3bc76f5bd47ad1a7
baldo-vela <74266847+baldo-vela@users.noreply.github.com>
2021-11-02 15:27:15 -0500

    TODOs a bug in descrption rendering

--- src/containers/PlaylistPreview.js
+++ src/containers/PlaylistPreview.js
@@ -11,12 +11,14 @@ class PlaylistPreview extends Component {
 
     // }
     render() {
+        //TODO: Fix issue with <p> tags in descrip:
+            //<p>Enter another dimension of classical music with these atmospheric Sci-fi soundtracks. Twin playlist: <a href="spotify:user:spotify:playlist:37i9dQZF1DX0i61tT0OnnK">Space-themed Classical Music</a>.</p>
         // { spotifyID, name: "Starlight Brigade Vibes", description: "Does what it says on the tin", owner: spotify username }
         const { id, name, description, owner, spotifyLink } = this.props.playlist;
         return(
             <card class='playlist-preview' key={`playlist-${id}`}>
                 <h1><href class='playlist-preview-name'>{name}</href></h1>
-                <p>{description}</p>
+                <p>{description ? description : '' }</p>
                 <p>{spotifyLink}</p>
                 <p>id: {id}</p>
                 <p>Owner: {owner.display_name}</p>
