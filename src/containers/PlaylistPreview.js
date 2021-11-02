c95fee09a06eda5bfef8af4f351d8f9e01a769c1
baldo-vela <74266847+baldo-vela@users.noreply.github.com>
2021-11-01 22:47:42 -0500

    Switches to cards for previews for eventual MUI

--- src/containers/PlaylistPreview.js
+++ src/containers/PlaylistPreview.js
@@ -6,13 +6,13 @@ class PlaylistPreview extends Component {
         // { spotifyID, name: "Starlight Brigade Vibes", description: "Does what it says on the tin", owner: spotify username }
         const { id, name, description, owner, spotifyLink } = this.props.playlist;
         return(
-            <div key={`playlist-${id}`}>
+            <card key={`playlist-${id}`}>
                 <h1>{name}</h1>
                 <p>{description}</p>
                 <p>{spotifyLink}</p>
                 <p>id: {id}</p>
                 <p>Owner: {owner.display_name}</p>
-            </div>
+            </card>
         )
     }
 }
