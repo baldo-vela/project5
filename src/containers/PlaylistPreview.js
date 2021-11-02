9adda0f82f9be88b025f0089175e151b83a59066
baldo-vela <74266847+baldo-vela@users.noreply.github.com>
2021-11-02 14:22:53 -0500

    Adds a loading state boolean, and styling classes

--- src/containers/PlaylistPreview.js
+++ src/containers/PlaylistPreview.js
@@ -1,13 +1,21 @@
 import React, {Component} from 'react'
 
 class PlaylistPreview extends Component {
+    constructor(){
+        super();
+        this.state = {
+            isLoading: true,
+        }
+    }
+    // linkToPlayer = () =>{
 
+    // }
     render() {
         // { spotifyID, name: "Starlight Brigade Vibes", description: "Does what it says on the tin", owner: spotify username }
         const { id, name, description, owner, spotifyLink } = this.props.playlist;
         return(
-            <card key={`playlist-${id}`}>
-                <h1>{name}</h1>
+            <card class='playlist-preview' key={`playlist-${id}`}>
+                <h1><href class='playlist-preview-name'>{name}</href></h1>
                 <p>{description}</p>
                 <p>{spotifyLink}</p>
                 <p>id: {id}</p>
