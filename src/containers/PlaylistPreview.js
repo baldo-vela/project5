66c7371a8ed979c4b013ec8e3b087f47222db66a
baldo-vela <74266847+baldo-vela@users.noreply.github.com>
2021-11-02 16:05:18 -0500

    Corrects an unrecognized tag error

--- src/containers/PlaylistPreview.js
+++ src/containers/PlaylistPreview.js
@@ -16,13 +16,13 @@ class PlaylistPreview extends Component {
         // { spotifyID, name: "Starlight Brigade Vibes", description: "Does what it says on the tin", owner: spotify username }
         const { id, name, description, owner, spotifyLink } = this.props.playlist;
         return(
-            <card class='playlist-preview' key={`playlist-${id}`}>
-                <h1><href class='playlist-preview-name'>{name}</href></h1>
+            <div className='playlist-preview' key={`playlist-${id}`}>
+                <h1><href className='playlist-preview-name'>{name}</href></h1>
                 <p>{description ? description : '' }</p>
                 <p>{spotifyLink}</p>
                 <p>id: {id}</p>
                 <p>Owner: {owner.display_name}</p>
-            </card>
+            </div>
         )
     }
 }
