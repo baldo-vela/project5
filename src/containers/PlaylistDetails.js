8a4c431c281fc98bc0b33ee3ee1fd555e45825ce
baldo-vela <74266847+baldo-vela@users.noreply.github.com>
2021-11-06 22:54:03 -0500

    Adds a dispatch for better action handling

--- src/containers/PlaylistDetails.js
+++ src/containers/PlaylistDetails.js
@@ -1,15 +1,20 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { setPlaylistId, clearPlaylistId } from "../redux/slices/currentPlaylistSlice"
 import Player from '../components/Player';
 
 const PlaylistDetails = (props) => {
     // const location = useLocation();
+    const dispatch = useDispatch();
+    //New WIP clears the associated store entry on the currently viewed playlist Id
+    //dispatch(clearPlaylistId());
     console.log("Logging Props", props);
     const currentPlaylistId = props.match.params.id;
     console.log("CurrentPlaylistId:", currentPlaylistId);
     const currentPlaylist = useSelector((state) => state.userPlaylistsReducer.playlists.items.find(p => p.id === currentPlaylistId))
     console.log("Current Playlist", currentPlaylist)
     const authToken = useSelector((state)=> state.userSliceReducer.authToken)
+    
     
     return (     
         <div className='playlist-details'>
