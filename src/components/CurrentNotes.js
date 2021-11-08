7aa8500b7ecfcb16e97e291060d673adf6104fa9
baldo-vela <74266847+baldo-vela@users.noreply.github.com>
2021-11-08 00:06:44 -0600

    Implements a list of notes

--- src/components/CurrentNotes.js
+++ src/components/CurrentNotes.js
@@ -1,18 +1,31 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { loadNotes, makeNote } from "../redux/slices/PlaylistNotesSlice"
+import { loadNotes, makeNote, deleteNote } from "../redux/slices/PlaylistNotesSlice";
+import CreateNote from './CreateNote';
+import Note from "./Note";
 
-const CurrentNotes = () => {
+function CurrentNotes(props) {
     const dispatch = useDispatch();
-    const currentNotes = useSelector(loadNotes);
+    const playlistId = props.currentPlaylist;
+    const currentNotes = [{ 
+        username: 'moleculetycoon', 
+        content: "Testing1", 
+        playlistId: playlistId,
+        date: null,
+    },]
+    // useDispatch(loadNotes(playlistId));
 
     const onMakeNoteHandler = (note) => {
         dispatch(makeNote(note));
     };
+    const handleDelete = dispatch(deleteNote);
 
     return currentNotes.map((note) => (
-        <div>
+        <div className={`playlist-${playlistId}-notes`}>
             <h1> A series of notes goes here.</h1>
+            <Note note={note} handleDelete={handleDelete}/>
+            <CreateNote onMakeNoteHandler={onMakeNoteHandler}/>
         </div>
     ));
-};
+}
+export default CurrentNotes;
