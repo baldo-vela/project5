41339bd0630efe7af1cd86326207aa24f4bcd6bd
baldo-vela <74266847+baldo-vela@users.noreply.github.com>
2021-11-08 14:35:55 -0600

    Implements jwt decode

--- src/actions/useAuth.js
+++ src/actions/useAuth.js
@@ -2,6 +2,11 @@ import { useState, useEffect } from "react"
 import { selectAuth } from "../redux/slices/userSlice"
 
 export default function useAuth(){
+    if (jwtDecode(token).exp < Date.now() / 1000) {
+        next(action);
+        localStorage.clear();
+      }
+      return
 
 
 }
