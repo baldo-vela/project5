e6a3f83568bd958c4b848439b454476e3f946451
baldo-vela <74266847+baldo-vela@users.noreply.github.com>
2021-11-08 22:23:38 -0600

    Refactors create and delete actions

--- src/components/CurrentNotes.js
+++ src/components/CurrentNotes.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect} from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { loadNotes, makeNote, deleteNote } from "../redux/slices/PlaylistNotesSlice";
 import CreateNote from './CreateNote';
@@ -7,6 +7,10 @@ import Note from "./Note";
 function CurrentNotes(props) {
     const dispatch = useDispatch();
     const playlistId = props.currentPlaylist;
+    // useEffect(()=> {
+    //     currentNotes = dispatch(loadNotes(playlistId))
+    //     console.log("Current Notes from store:", currentNotes)
+    // })
     const currentNotes = [{ 
         username: 'moleculetycoon', 
         content: "Testing1", 
@@ -18,12 +22,12 @@ function CurrentNotes(props) {
     const onMakeNoteHandler = (note) => {
         dispatch(makeNote(note));
     };
-    const handleDelete = dispatch(deleteNote);
+    
 
     return currentNotes.map((note) => (
         <div className={`playlist-${playlistId}-notes`}>
             <h1> A series of notes goes here.</h1>
-            <Note note={note} handleDelete={handleDelete}/>
+            <Note note={note} handleDelete={()=> dispatch(deleteNote(note))}/>
             <CreateNote onMakeNoteHandler={onMakeNoteHandler}/>
         </div>
     ));
