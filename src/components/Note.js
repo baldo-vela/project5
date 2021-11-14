fab0f78dbb9d87410b17b1115d0c4f98e00df4d1
baldo-vela <74266847+baldo-vela@users.noreply.github.com>
2021-11-14 16:24:47 -0600

    Cleans up some unused imports

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,12 +1,14 @@
 import React from "react";
-import { useDispatch } from "react-redux";
+import { 
+    // useDispatch 
+} from "react-redux";
 //MUI Imports
-import { createStyles, makeStyles } from '@mui/styles';
-import Box from "@mui/material/Box";
+// import { createStyles, makeStyles } from '@mui/styles';
+// import Box from "@mui/material/Box";
 import Card from "@mui/material/Card";
 import CardHeader from "@mui/material/CardHeader";
 import CardContent from "@mui/material/CardContent";
-import CardActions from "@mui/material/CardActions";
+// import CardActions from "@mui/material/CardActions";
 import DeleteOutlined from '@mui/icons-material//DeleteOutlined'
 import Typography from "@mui/material/Typography";
 import { ClassNames } from "@emotion/react";
